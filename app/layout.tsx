d398b7bdf2f300370994a1eafa19a8635c77cfc2
ZiiiMaschine <wootsat@gmail.com>
2025-12-23 12:00:37 -0600

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,21 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react" // <--- 1. Import this
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'TGTBT',
-  description: 'Too Good To Be True - Rate and Discover Viral Videos',
+  description: 'Too Good To Be True',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics /> {/* <--- 2. Add this component here */}
+      </body>
     </html>
   )
 }
